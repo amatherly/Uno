d7b48ec046ef0da1d4db8873b8879b36fc8f7f64
amatherly <69179454+amatherly@users.noreply.github.com>
2021-11-12 19:46:51 -0800

    switched the innards of card and deck

--- src/Deck.java
+++ src/Deck.java
@@ -1,16 +1,26 @@
 public class Deck {
+
+
+
     public Deck() {
+        //constructor
+        //populate a list of all the cards in the deck with the Card class
     }
 
     public void shuffle(){
+        // take the list of cards in the deck
 
     }
 
-    public void deal(Player player){
+    public void deal(){
+        // pop 7 cards off the top
+        // for(player : MainGame)?
+
 
     }
 
     public void draw (int numberOfCards){
+        // pop one card off the deck
 
     }
 }
