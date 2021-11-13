713e1fe791a8d434d3631af05ca1820f8b8e2892
amatherly <69179454+amatherly@users.noreply.github.com>
2021-11-12 22:12:59 -0800

    switched the innards of card and deck

--- src/Card.java
+++ src/Card.java
@@ -1,8 +1,10 @@
 public class Card {
 
     int data;
+    int color;
 
-    public Card(int data) {
+    public Card(int data, int color) {
         this.data = data;
+        this.color = color;
     }
 }
--- src/Deck.java
+++ src/Deck.java
@@ -1,11 +1,36 @@
-import java.util.LinkedList;
+import java.util.HashSet;
+import java.util.Set;
 
-public class Deck {
+public class Deck implements{
 
-    LinkedList<Card> deck = new LinkedList<Card>();
+    Set<Card> deck = new HashSet<Card>();
 
 
-    public Deck (Card card) {
+    public Deck () {
+       // will populate deck with each card
+        for (int i = 0; i < 108; i++) {
+            for (int j = 0; j < 4; j++) {
+                Card card = new Card(i, j);
+                deck.add(card);
+            }
+
+        }
+
+        /*
+        Deck has 108 cards total
+        -19 Blue
+        -19 Yellow
+        -19 Red
+        -19 Green
+        8 Skip
+        8 Reverse
+        8 Draw Two
+        4 Wild
+        4 Wild Draw 4
+        */
+
+
+
 
         //constructor
         //populate a list of all the cards in the deck with the Card class
