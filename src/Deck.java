4a5e6b62157abbf657f4ad1831ae6993db06c758
amatherly <69179454+amatherly@users.noreply.github.com>
2021-11-12 19:42:47 -0800

    switched the innards of card and deck

--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,16 @@
 public class Deck {
+    public Deck() {
+    }
+
+    public void shuffle(){
+
+    }
+
+    public void deal(Player player){
+
+    }
+
+    public void draw (int numberOfCards){
+
+    }
 }
