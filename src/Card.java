f21237ef57bb8fdf20b080086ab7ef3c5725fc31
amatherly <69179454+amatherly@users.noreply.github.com>
2021-11-12 18:27:52 -0800

    creating Classes

---
+++ src/Card.java
@@ -0,0 +1,16 @@
+public class Card {
+    public Card() {
+    }
+
+    public void shuffle(){
+
+    }
+
+    public void deal(Player player){
+
+    }
+
+    public void draw (int numberOfCards){
+
+    }
+}
