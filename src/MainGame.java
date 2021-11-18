a1e9918cf71baecf0f4c9e72460829b4b486d752
LinusM24 <94580290+LinusM24@users.noreply.github.com>
2021-11-17 17:22:41 -0800

    Update MainGame.java

--- src/MainGame.java
+++ src/MainGame.java
@@ -1,3 +1,25 @@
 public class MainGame {
+private int currentPlayers;
+	private String[] PlayerNames;
+	
+	private UnoDeck deck;// For deck class that needs to be implemented
+	private ArrayList<ArrayList<UnoCard>>CardsPlayer;
+	private ArrayList<UnoCard>CardPile;
+	
+	private UnoCard.Color Color;
+	private UnoCard.Value Value;
+	
+	boolean cardDirection;
+	
+	public Game(String[]names)
+	{
+		deck=new UnoDeck();
+		deck.random();
+		CardPile= new ArrayList<UnoCard>();
+		PlayerNames=names;
+		currentPlayers=0;
+		cardDirection=false;
+		
+	}
 
 }
