938d5b4f9ae307208393d33f0165acdfefa44ef5
clickmee <94204735+clickmee@users.noreply.github.com>
2021-11-22 18:19:46 -0800

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,47 @@
+import java.util.ArrayList;
+
 public class Player {
+	// each player will have a "hands" of an ArrayList that contain Card class object
+	private ArrayList<Card> player;
 
+	// create a player with empty ArrayList of Card objects
+	public Player() {
+		this.player = new ArrayList<Card>();
+	}
 
+	/**
+	 *  add a card into the "hands"
+	 *  
+	 * @param card
+	 */
+	public void add(Card card) {
+		this.player.add(card);
+	}
+	
+	/**
+	 *  play a card out of the "hands"
+	 *  this is zero-based for now
+	 *  
+	 * @param i
+	 */
+	public void play(int i) {
+		this.player.remove(i);
+	}
+	
+	
+	/**
+	 * This will show all the card in a player hand
+	 */
+	public void hand() {
+		System.out.print("[");
+		for (int i = 0; i < player.size(); i++) {
+			if(i == player.size() - 1) {
+				System.out.print(player.get(i));
+			}
+			else {
+				System.out.print(player.get(i) + ", ");
+			}
+		}
+		System.out.print("]");
+	}
 }
